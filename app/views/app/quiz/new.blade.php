145a74e242c422642aa95e468cd1bbae5696953a
Kunal Varma <kunalvarma05@gmail.com>
2015-01-20 03:20:51 +0530

    Tweaks

--- app/views/app/quiz/new.blade.php
+++ app/views/app/quiz/new.blade.php
@@ -2,11 +2,11 @@
 @section('main')
 <div class="new-quiz-page">
 	<div class="new-quiz-chapters">
-		<div class="alert alert-block alert-info">
+		<div class="panel panel-default well-sm">
 			Select the chapters you want to include the questions from, in the quiz
 		</div>
 		{{Form::open(array('route' => "app.quiz.generate"))}}
-		{{Form::select('chapters[]', $chapters, array(), array('class' => "form-control input-lg", 'required' => "required", 'placeholder' => "Chapters", 'rows' => '6', 'multiple' => "multiple"))}}
+		{{Form::select('chapters[]', $chapters, array(), array('class' => "form-control input-lg", 'required' => "required", 'placeholder' => "Chapters", 'rows' => '6', 'multiple' => "multiple", 'data-selector' => "true"))}}
 		{{Form::submit('Start Quiz', array('class' => "btn btn-lg btn-success btn-block"))}}
 		{{Form::close()}}
 	</div>
