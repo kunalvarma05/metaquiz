5303a714e64087a617a0ff5e0898b318341f5458
Kunal Varma <kunalvarma05@gmail.com>
2015-01-20 04:36:30 +0530

    Removed test route

--- app/routes.php
+++ app/routes.php
@@ -1,30 +1,4 @@
 <?php
-
-Route::get('test', function(){
-// since we connect to default setting localhost
-// and 6379 port there is no need for extra
-// configuration. If not then you can specify the
-// scheme, host and port to connect as an array
-// to the constructor.
-	Debugbar::measure('publish', function(){
-		try {
-			$single_server = array(
-				'host'     => '127.0.0.1',
-				'port'     => 6379,
-				'password' => "K@NT@R@NIV@RM@7600965334"
-				);
-
-			$client = new Predis\Client($single_server);
-			$pubsub = $client->pubSub();
-			$client->publish("realtime", Auth::check() ? Auth::user()->id : rand(0,20));
-			echo "done";
-		}
-		catch (Exception $e) {
-			echo "Couldn't connected to Redis";
-			echo $e->getMessage();
-		}
-	});
-});
 /**
  * The Base Route
  */
