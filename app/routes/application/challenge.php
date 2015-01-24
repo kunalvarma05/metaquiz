8c8b665f12bc1df745af3423936b0eb965ba093a
Kunal Varma <kunalvarma05@gmail.com>
2015-01-25 02:38:15 +0530

    Challenge Routes

--- app/routes/application/challenge.php
+++ app/routes/application/challenge.php
@@ -2,9 +2,31 @@
 /**
 * Challenge Routes
 */
+Route::group(array('prefix' => "challenges", 'before' => "auth|has-password|activated"), function() {
 
+	/**
+	 * All Challenges
+	 */
+	Route::get('/', array('as' => "app.challenges", 'uses' => "ChallengeController@index") );
 
-/**
- * Create Challenge
- */
-Route::post('challenge/create', array('as' => "app.quiz.create-challenge", 'uses' => "ChallengeController@create"));
+	/**
+	 * Create Challenge
+	 */
+	Route::post('create', array('as' => "app.challenges.create", 'uses' => "ChallengeController@create"));
+
+	/**
+	 * Single Challenge Page
+	 */
+	Route::get('{id}', array('as' => "app.challenges.show", 'uses' => "ChallengeController@show"));
+
+	/**
+	 * Accept Challenge
+	 */
+	Route::post('accept', array('as' => "app.challenges.accept", 'uses' => "ChallengeController@accept"));
+
+	/**
+	 * Reject Challenge
+	 */
+	Route::post('reject', array('as' => "app.challenges.reject", 'uses' => "ChallengeController@reject"));
+
+});
