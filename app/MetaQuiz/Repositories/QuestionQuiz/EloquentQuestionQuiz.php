5cfafa9797df353d728b549048672cf7d810c33d
Kunal Varma <kunalvarma05@gmail.com>
2015-01-27 06:27:03 +0530

    QuestionQuiz Repository and Interface

---
+++ app/MetaQuiz/Repositories/QuestionQuiz/EloquentQuestionQuiz.php
@@ -0,0 +1,34 @@
+<?php
+namespace MetaQuiz\Repositories\QuestionQuiz;
+
+use MetaQuiz\Repositories\AbstractEloquentRepository;
+use MetaQuiz\Service\Cache\CacheInterface;
+use Illuminate\Database\Eloquent\Model;
+
+class EloquentQuestionQuiz extends AbstractEloquentRepository implements QuestionQuizInterface {
+
+	/**
+	 * $model The Eloquent model
+	 * @var Illuminate\Database\Eloquent\Model
+	 */
+	protected $model;
+
+	/**
+	 * $cache The Cache Interface
+	 * @var MetaQuiz\Service\Cache\CacheInterface
+	 */
+	protected $cache;
+
+	/**
+	 * __construct Create a new Eloquent QuestionQuiz
+	 * @param Illuminate\Database\Eloquent\Model          $model The Eloquent Model
+	 * @param MetaQuiz\Service\Cache\CacheInterface $cache The Cache Interface
+	 * @return void
+	 */
+	public function __construct(Model $model, CacheInterface $cache) {
+		//Set the object
+		$this -> model = $model;
+		$this -> cache = $cache;
+	}
+
+}
---
+++ app/MetaQuiz/Repositories/QuestionQuiz/QuestionQuizInterface.php
@@ -0,0 +1,9 @@
+<?php
+namespace MetaQuiz\Repositories\QuestionQuiz;
+
+use MetaQuiz\Repositories\RepositoryInterface;
+
+interface QuestionQuizInterface extends RepositoryInterface {
+
+
+}
