5e6a4856c4a1d4eca53399c967839db3b876a25d
Kunal Varma <kunalvarma05@gmail.com>
2015-02-02 07:23:56 +0530

    Remove History Menu Item

--- app/views/app/partials/main-navigation.blade.php
+++ app/views/app/partials/main-navigation.blade.php
@@ -10,7 +10,7 @@
 			<a href="{{route('app.subjects')}}" data-toggle="tooltip" data-placement="right" class="icon-grid subject-icon" title="Subjects"></a>
 		</li>
 		<li class="{{HTML::activeState('app.quizzes')}}">
-			<a href="{{route('app.quizzes')}}" data-toggle="tooltip" data-placement="right" class="icon-pencil quiz-icon" title="Challenges"></a>
+			<a href="{{route('app.quizzes')}}" data-toggle="tooltip" data-placement="right" class="icon-pencil quiz-icon" title="Quizzes"></a>
 		</li>
 		<li class="{{HTML::activeState('app.challenges')}}">
 			<a href="{{route('app.challenges')}}" data-toggle="tooltip" data-placement="right" class="icon-fire challenge-icon" title="Challenges"></a>
@@ -23,9 +23,6 @@
 			<span class="count">{{$friend_requests}}</span>
 			@endif
 			<a href="{{route('app.friends')}}" data-toggle="tooltip" data-placement="right" class="icon-users friend-icon" title="Friends"></a>
-		</li>
-		<li>
-			<a href="#" data-toggle="tooltip" data-placement="right" class="icon-calendar history-icon" title="History"></a>
 		</li>
 		<li>
 			<a href="#" data-toggle="tooltip" data-placement="right" class="icon-bar-chart ranking-icon" title="Rankings"></a>
