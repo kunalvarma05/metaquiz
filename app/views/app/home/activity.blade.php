0b80f038feb8750878f17cf03f7917527df0c337
Kunal Varma <kunalvarma05@gmail.com>
2015-02-02 06:14:07 +0530

    Quiz Start activity

--- app/views/app/home/activity.blade.php
+++ app/views/app/home/activity.blade.php
@@ -3,6 +3,8 @@
 	<div class="activity-item">
 		@if($activity->type === "quiz_win")
 			@include('app.partials.activity.quiz_win')
+		@elseif($activity->type === "quiz_start")
+			@include('app.partials.activity.quiz_start')
 		@endif
 	</div>
 	@endforeach
