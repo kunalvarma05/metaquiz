a3fdee0a8e10793481dd97dabec036ff019b3a4f
Kunal Varma <kunalvarma05@gmail.com>
2015-07-01 01:19:01 +0530

    Homestead config

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('CreationMachine'),
+	'local' => array('CreationMachine','homestead'),
 
 ));
 
